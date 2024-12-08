23ea81a787a0332870e1706ee1a57c742fcfb9a6
reenat <reenat056@yandex.ru>
2024-12-08 21:16:46 +0300

    добавил иконку приложения

--- app/src/main/java/ru/netology/nework/activity/MainActivity.kt
+++ app/src/main/java/ru/netology/nework/activity/MainActivity.kt
@@ -1,10 +1,11 @@
-package ru.netology.nework
+package ru.netology.nework.activity
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import ru.netology.nework.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
